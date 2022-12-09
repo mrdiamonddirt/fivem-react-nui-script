b4b41f9623ffc6fd86d3522f11e111060036158e
RowDog <mrdiamonddirt@gmail.com>
2022-12-09 22:27:03 +0000

    added little window

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,72 +1,91 @@
-import React, {useState} from 'react';
-import './App.css'
-import {debugData} from "../utils/debugData";
-import {fetchNui} from "../utils/fetchNui";
+import React, { useState } from "react";
+import "./App.css";
+import { debugData } from "../utils/debugData";
+import { fetchNui } from "../utils/fetchNui";
 
 // This will set the NUI to visible if we are
 // developing in browser
 debugData([
-  {
-    action: 'setVisible',
-    data: true,
-  }
-])
+    {
+        action: "setVisible",
+        data: true,
+    },
+]);
 
 interface ReturnClientDataCompProps {
-  data: any
+    data: any;
 }
 
-const ReturnClientDataComp: React.FC<ReturnClientDataCompProps> = ({data}) => (
-  <>
-    <h5>Returned Data:</h5>
-    <pre>
-      <code>
-        {JSON.stringify(data, null)}
-      </code>
-    </pre>
-  </>
-)
+const ReturnClientDataComp: React.FC<ReturnClientDataCompProps> = ({
+    data,
+}) => (
+    <>
+        <h5>Returned Data:</h5>
+        <pre>
+            <code>{JSON.stringify(data, null)}</code>
+        </pre>
+    </>
+);
 
 interface ReturnData {
-  x: number;
-  y: number;
-  z: number;
+    x: number;
+    y: number;
+    z: number;
 }
 
 const App: React.FC = () => {
-  const [clientData, setClientData] = useState<ReturnData | null>(null)
+    const [clientData, setClientData] = useState<ReturnData | null>(null);
 
-  const handleGetClientData = () => {
-    fetchNui<ReturnData>('getClientData').then(retData => {
-      console.log('Got return data from client scripts:')
-      console.dir(retData)
-      setClientData(retData)
-    }).catch(e => {
-      console.error('Setting mock data due to error', e)
-      setClientData({ x: 500, y: 300, z: 200})
-    })
-  }
+    const handleGetClientData = () => {
+        fetchNui<ReturnData>("getClientData")
+            .then((retData) => {
+                console.log("Got return data from client scripts:");
+                console.dir(retData);
+                setClientData(retData);
+            })
+            .catch((e) => {
+                console.error("Setting mock data due to error", e);
+                setClientData({ x: 500, y: 300, z: 200 });
+            });
+    };
 
-  const handleCreateNewNui = () => {
-  }
+    const [opentab, setopentab] = useState(false);
 
-  return (
-    <div className="nui-wrapper">
-      <div className='popup-thing'>
-        <div>
-          <h1>This is the NUI Popup!</h1>
-          <p>Exit with the escape key</p>
-          <button onClick={handleGetClientData}>Get Client Data</button>
-          <button onClick={handleCreateNewNui}>Open Another Window</button>
-          {clientData && <ReturnClientDataComp data={clientData} />}
+    const handleCreateNewNui = () => {
+        setopentab(true);
+        console.log("Opening new NUI", opentab);
+    };
+
+    const closewindow = () => {
+        setopentab(false);
+        console.log("Closing NUI", opentab);
+    };
+
+    return (
+        <div className="nui-wrapper">
+            <div className="popup-thing">
+                <div>
+                    <h1>This is the NUI Popup!</h1>
+                    <p>Exit with the escape key</p>
+                    <button onClick={handleGetClientData}>
+                        Get Client Data
+                    </button>
+                    <button onClick={handleCreateNewNui}>
+                        Open Another Window
+                    </button>
+                    {clientData && <ReturnClientDataComp data={clientData} />}
+                </div>
+            </div>
+            {opentab ? (
+                <div className="popup-thing2">
+                    <div>
+                        <h1>This is another NUI Popup!</h1>
+                        <button onClick={closewindow}>close</button>
+                    </div>
+                </div>
+            ) : null}
         </div>
-      </div>
-      <div className='another-popup'>
-        <h1>This is another Element</h1>
-        <button>Close</button>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
 export default App;
